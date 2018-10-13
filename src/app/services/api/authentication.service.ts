f0ba4e893a3787357da2f270f36f6bd4dd2661ca
Sander <sanderbrokelman@msn.com>
2018-10-13 15:40:14 +0200

    Updated to angular 6.1.10

--- src/app/services/api/authentication.service.ts
+++ src/app/services/api/authentication.service.ts
@@ -1,7 +1,7 @@
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+
 import { environment } from '../../../environments/environment';
 
 @Injectable()
